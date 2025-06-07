68f0ec5511efa95ecdedc656cf9ec2f4e285c23f
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-06-08 04:16:54 +0800

    Complete questions in files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -6,7 +6,7 @@ out_file.close()
 
 # Q2
 in_file = open("name.txt", "r")
-name = in_file.read().strip()  # strip to remove any extra newline
+name = in_file.read().strip()
 in_file.close()
 print(f"Hi {name}!")
 
@@ -15,11 +15,11 @@ with open("numbers.txt", "r") as in_file:
     first_number = int(in_file.readline())
     second_number = int(in_file.readline())
     result = first_number + second_number
-    print(result)  # Should print 59 if numbers.txt contains 17, 42, 400
+    print(result)
 
 # Q4
 with open("numbers.txt", "r") as in_file:
     total = 0
     for line in in_file:
         total += int(line)
-    print(total)  # Should print 459 if file has 17, 42, 400
+    print(total)
