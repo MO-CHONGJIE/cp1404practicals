9614a73c0a5e8e2ba2931ffec12ce5a1e70a729f
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-06-15 04:02:11 +0800

    Create a program to get user input, randomly generate numbers and output them

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,7 +7,7 @@ MAX = 45
 
 def main():
     number_of_picks = int(input("How many quick picks? "))
-    for i in range(number_of_picks):
+    for _ in range(number_of_picks):
         quick_pick = []
         while len(quick_pick) < NUMBERS_PER_LINE:
             number = random.randint(MIN, MAX)
