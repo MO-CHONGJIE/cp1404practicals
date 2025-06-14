01bd20028298241dd2825f9d28d5109713717ded
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-06-15 03:28:28 +0800

    Code now can covert data in txt file and covert it into list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,23 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Load data from file and display subject information."""
     data = load_data()
-    print(data)
+
 
 
 def load_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Read data from file formatted """
+    subject_data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])  # Convert student count to int
+        subject_data.append(parts)
     input_file.close()
+    return subject_data
+
 
 
 main()
