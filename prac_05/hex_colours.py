e09502e1727fe7cbc76c1e27821b9321603f6a66
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-06-21 02:41:49 +0800

    Change dictionary name

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,4 +1,4 @@
-COLOUR_AND_CODE = {"BLACK": "#000000", "BRONZE": "#cd7f32", "ALICEBLUE": "#f0f8ff",
+COLOUR_TO_CODE = {"BLACK": "#000000", "BRONZE": "#cd7f32", "ALICEBLUE": "#f0f8ff",
                   "BLOND": "#faf0be", "AMARANTH": "#e52b50", "AMBER": "#ffbf00",
                   "AMETHYST": "#9966cc", "BISTRE": "#3d2b1f", "BROWN": "#a52a2a",
                   "BRASS": "#b5a642"}
@@ -6,7 +6,7 @@ COLOUR_AND_CODE = {"BLACK": "#000000", "BRONZE": "#cd7f32", "ALICEBLUE": "#f0f8f
 user_choice = input("Enter colour:").upper()
 while user_choice != "":
     try:
-        print(f"{user_choice} is {COLOUR_AND_CODE[user_choice]}")
+        print(f"{user_choice} is {COLOUR_TO_CODE[user_choice]}")
 
     except KeyError:
         print("Invalid colour")
