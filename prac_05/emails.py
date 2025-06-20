8639b003f46b94b72f7384b3e8b6e52a0994d928
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-06-21 04:05:15 +0800

    get username if email not equal to username, and print result

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,17 +5,29 @@ Actual:  minutes
 
 """
 
+
 def main():
     email_to_name = {}
     email = input("Email: ")
     while email != "":
         name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ").lower()
+        if confirmation not in ["", "y"]:
+            name = input("Name: ")
+
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
 
 def get_name_from_email(email):
-    """get username from email address."""
+    """Get name from email address"""
     username = email.split('@')[0]
     name_parts = username.replace('.', ' ').split()
     name = ' '.join(name_parts).title()
     return name
+
 
 main()
