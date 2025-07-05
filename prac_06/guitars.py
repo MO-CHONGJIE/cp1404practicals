2db54806ec9076fa90c55d627df02853fbdd7007
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-06 03:06:40 +0800

    Determine the status of the guitar(vintage?) and print all the user's input.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,6 @@
 """
 guitars
-Estimate:  30 minutes
+Estimate: 30 minutes
 Actual:    minutes
 """
 
@@ -19,5 +19,10 @@ def main():
         guitars.append(guitar)
         print(f"{guitar} added.\n")
         name = input("Name: ")
+
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 main()
