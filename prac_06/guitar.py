de32a72c68c4eccc3a86db5062bd47a7e22752ef
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-06 01:55:44 +0800

    Create a new class

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""
+guitar
+Estimate:  minutes
+Actual:    minutes
+"""
+
+CURRENT_YEAR = 2022
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar ."""
+        self.name = name
+        self.year = year
+        self.cost = cost
