5dbb7ebcf5e029860ac2262173071fa6f16631df
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-06 03:04:30 +0800

    Can add new guitar as list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,6 @@
 """
 guitars
-Estimate:  minutes
+Estimate:  30 minutes
 Actual:    minutes
 """
 
@@ -9,3 +9,15 @@ from prac_06.guitar import Guitar
 def main():
     guitars = []
     print("My guitars!")
+
+    name = input("Name: ")
+
+    while name:
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
+
+main()
