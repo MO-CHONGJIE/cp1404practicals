95147064964d9b8b656ca58c1779ceabfe25816c
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-05 23:26:52 +0800

    Create fields by __init__

---
+++ prac_06/programming_language.py
@@ -0,0 +1,13 @@
+"""
+ProgrammingLanguage
+Estimate:  minutes
+Actual:   minutes
+"""
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a ProgrammingLanguage."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
