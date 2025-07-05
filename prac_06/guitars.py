33e915b3252f3fe984b8b2b5edfa7d5f07d80a27
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-06 03:08:11 +0800

    Add second way to get input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -11,6 +11,9 @@ def main():
     print("My guitars!")
 
     name = input("Name: ")
+    #Can use the following data to get input
+    """guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))"""
 
     while name:
         year = int(input("Year: "))
