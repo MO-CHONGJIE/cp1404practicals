2e7a122493bbe95e20f4f1cb590525de4577ce8c
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-12 22:37:16 +0800

    Read from txt file

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,3 +3,18 @@ project_management
 Estimate: 65 minutes
 Actual:  minutes
 """
+
+from project import Project
+from datetime import datetime
+
+FILENAME = "projects.txt"
+
+
+def load_projects(filename):
+    projects = []
+    with open(filename, "r") as file:
+        next(file)
+        for line in file:
+            parts = line.strip().split(',')
+            projects.append(Project)
+    return projects
