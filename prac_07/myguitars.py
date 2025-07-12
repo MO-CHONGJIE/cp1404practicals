4e0b6e72c520e073cba3df51a4b753519b186c27
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-12 19:43:00 +0800

    Create main function

---
+++ prac_07/myguitars.py
@@ -0,0 +1,16 @@
+import csv
+from guitar import Guitar
+
+
+def main():
+    """Load from a CSV file and return a list ."""
+    guitars = []
+    in_file = open('guitars.csv', 'r')
+    for line in in_file:
+        parts = line.strip().split(',')
+        name = parts[0]
+        year = int(parts[1])
+        cost = float(parts[2])
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+    in_file.close()
