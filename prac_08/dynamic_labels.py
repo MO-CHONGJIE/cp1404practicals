027e9fb717f2d02d5e55c1b7c7164c6254ed001c
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-20 06:13:48 +0800

    Dynamically creates a separate Label for each one.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -5,6 +5,8 @@ Actual:  minutes
 """
 
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
 
 class DynamicLabelsApp(App):
     def __init__(self, **kwargs):
@@ -12,6 +14,13 @@ class DynamicLabelsApp(App):
         super().__init__(**kwargs)
         self.names = ["John", "Lily", "Clara", "Rose"]
 
-
+    def build(self):
+        """Build the kivy GUI"""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file("dynamic_labels.kv")
+        for name in self.names:
+            label = Label(text=name)
+            self.root.ids.main.add_widget(label)
+        return self.root
 
 DynamicLabelsApp().run()
