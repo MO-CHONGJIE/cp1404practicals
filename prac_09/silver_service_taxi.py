4f9c08869797af1761bb281496cb7dfff27f1a54
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-26 21:25:31 +0800

    print as output sample by reuse parent's class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -7,3 +7,8 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        """Return a string representation of a SilverServiceTaxi"""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
