d0fc4d4e7e2d3f54598c01a16cff2274e5950be2
MO-CHONGJIE <chongjie.mo@my.jcu.edu.au>
2025-07-26 22:27:20 +0800

    Delete import Car, useless code

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,19 +1,25 @@
 """
 Taxi_simulator
-Estimate: 85 minutes
-Actual:
+Estimate: 1.5h
+Actual: 1.8h
 """
 
-from car import Car
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
+
 def main():
-    """A taxi simulator program matches readme.md's requirements"""
+    """A taxi simulator program matching README.md's requirements."""
     total_bill = 0
     current_taxi = None
-    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo",100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    taxis = [Taxi("Prius", 100),
+             SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+
     print("Let's drive!")
     print(MENU)
+    menu_choice = input(">>> ").lower()
+
+main()
